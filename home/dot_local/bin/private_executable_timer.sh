8b00992ee09ac73f98bda38db4f769ff2918e775
Ian Curran <icurran@protonmail.com>
2023-05-28 18:14:43 -0400

    added go path and changed timer script

--- home/dot_local/bin/private_executable_timer.sh
+++ home/dot_local/bin/private_executable_timer.sh
@@ -4,9 +4,15 @@ con=0
 while [ $con = 0 ]; do
       choice=$(echo "Work\nSmall Break\nLong Break\nExit" | fzf)
       case $choice in 
-	      Work) termdown 25m -t "Take A Break" ;;
-	      "Small Break") termdown 5m -t "Break is Over!";;
-	      "Long Break") termdown 15m -t "Break is Over!";;
+	      Work) termdown 25m 
+		             mpv --no-video "$XDG_DATA_HOME/audio/alarm-sounds/hotel-service-bell.mp3" >/dev/null 2>&1 &
+		             termdown -T "Take A Break";;
+	      "Small Break") termdown 5m
+			     mpv --no-video "$XDG_DATA_HOME/audio/alarm-sounds/analog-alarm-clock.mp3" >/dev/null 2>&1 &
+			     termdown -T "Break is Over!";;
+	      "Long Break") termdown 15m
+			     mpv --no-video "$XDG_DATA_HOME/audio/alarm-sounds/analog-alarm-clock.mp3" >/dev/null 2>&1 &
+			     termdown -T "Break is Over!";;
 	      Exit) con=$(($con+1))
       esac
 done
