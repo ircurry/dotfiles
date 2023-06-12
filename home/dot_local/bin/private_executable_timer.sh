b97ff2cc02919f891b5fcb35b5e8e2ec8caf0d95
Ian Curran <icurran@protonmail.com>
2023-06-12 16:13:19 -0400

    added dunst notifications to pomodoro script

--- home/dot_local/bin/private_executable_timer.sh
+++ home/dot_local/bin/private_executable_timer.sh
@@ -5,14 +5,17 @@ while [ $con = 0 ]; do
       choice=$(echo "Work\nSmall Break\nLong Break\nExit" | fzf)
       case $choice in 
 	      Work) termdown 25m 
-		             mpv --no-video "$XDG_DATA_HOME/audio/alarm-sounds/hotel-service-bell.mp3" >/dev/null 2>&1 &
-		             termdown -T "Take A Break";;
+		    mpv --no-video "$XDG_DATA_HOME/audio/alarm-sounds/hotel-service-bell.mp3" >/dev/null 2>&1 &
+		    dunstify -i alarm-symbolic "Work Session Over" "Take a break!"
+		    termdown -T "Take A Break";;
 	      "Small Break") termdown 5m
 			     mpv --no-video "$XDG_DATA_HOME/audio/alarm-sounds/analog-alarm-clock.mp3" >/dev/null 2>&1 &
+			     dunstify -i alarm-symbolic "Break is Over" "Get Back To Work!" -u critical
 			     termdown -T "Break is Over!";;
 	      "Long Break") termdown 15m
-			     mpv --no-video "$XDG_DATA_HOME/audio/alarm-sounds/analog-alarm-clock.mp3" >/dev/null 2>&1 &
-			     termdown -T "Break is Over!";;
+			    mpv --no-video "$XDG_DATA_HOME/audio/alarm-sounds/analog-alarm-clock.mp3" >/dev/null 2>&1 &
+			    dunstify -i alarm-symbolic "Break is Over" "Get Back To Work!" -u critical
+			    termdown -T "Break is Over!";;
 	      Exit) con=$(($con+1))
       esac
 done
